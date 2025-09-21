f886e92daa1a7438e69a5aeea334cdb36c98e646
borisrosedev <boris.rose.dev@gmail.com>
2025-09-21 10:56:15 +0200

    feat/my-tp1-done

--- database/seed.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE glpi CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'glpiuser'@'localhost' IDENTIFIED BY 'caroline123';
-GRANT ALL PRIVILEGES ON glpi.* TO 'glpiuser'@'localhost';
-FLUSH PRIVILEGES;
-EXIT;
